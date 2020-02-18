fbd1b3b9e8bdcea9a4e8479550f83019d1ac205c
Yeison Gutierrez <ygutierrez@quqo.com>
2020-02-17 23:10:43 -0500

    Cambiar ruta de prod

--- src/constant/base.js
+++ src/constant/base.js
@@ -1,4 +1,4 @@
 export const PROCESS_ENV = process.env.NODE_ENV;
-export const HOST_SITE_ENDPOINTS = "http://localhost:5000/v1";
+export const HOST_SITE_ENDPOINTS = "https://motor-api.herokuapp.com/v1";
 export const JWT_SECRET = "secret";
 export const API_KEY_GOOGLE_MAPS = "AIzaSyAdcwo4RNl9PlEfTvnnlexy8bLVavGRhV8";
